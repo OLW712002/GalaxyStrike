ddcc078958069aa56f10d1812d992e2becefc2a2
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2025-06-25 09:05:24 +0700

    Update PlayerMovement.cs
    Add movement logic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,20 +3,19 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] float movespeed = 10f;
+    Vector2 movement;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float xOffset = movement.x * movespeed * Time.deltaTime;
+        float yOffset = movement.y * movespeed * Time.deltaTime;
+        transform.localPosition = new Vector3(transform.localPosition.x + xOffset, transform.localPosition.y + yOffset, 0f);
     }
 
     void OnMove(InputValue value)
     {
         Debug.Log(value.Get<Vector2>());
+        movement = value.Get<Vector2>();
     }
 }
