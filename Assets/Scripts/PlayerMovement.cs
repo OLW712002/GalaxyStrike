4e0701b201e29dc60f962a455f6f59cd3a8645c4
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2025-06-26 08:56:27 +0700

    Clamp player move range

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,19 +3,36 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("MoveRange")]
+    [SerializeField] float xClampRange = 10f;
+    [SerializeField] float yClampRangeUp = 10f;
+    [SerializeField] float yClampRangeDown = 10f;
+
+    [Header("")]
     [SerializeField] float movespeed = 10f;
     Vector2 movement;
 
     void Update()
     {
-        float xOffset = movement.x * movespeed * Time.deltaTime;
-        float yOffset = movement.y * movespeed * Time.deltaTime;
-        transform.localPosition = new Vector3(transform.localPosition.x + xOffset, transform.localPosition.y + yOffset, 0f);
+        ProcessTranslating();
     }
 
     void OnMove(InputValue value)
     {
         Debug.Log(value.Get<Vector2>());
         movement = value.Get<Vector2>();
+    }
+
+    private void ProcessTranslating()
+    {
+        float xOffset = movement.x * movespeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xClampRange, xClampRange);
+
+        float yOffset = movement.y * movespeed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yClampRangeDown, yClampRangeUp);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, 0f);
     }
 }
