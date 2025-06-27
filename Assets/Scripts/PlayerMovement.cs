04e5512b639bf2d07450b4013904dd521c21a34b
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2025-06-27 22:27:52 +0700

    Update PlayerMovement.cs
    Add rotate when moving

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,18 +3,23 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float movespeed = 10f;
+
     [Header("MoveRange")]
     [SerializeField] float xClampRange = 10f;
     [SerializeField] float yClampRangeUp = 10f;
     [SerializeField] float yClampRangeDown = 10f;
 
-    [Header("")]
-    [SerializeField] float movespeed = 10f;
+    [Header("Rotation")]
+    [SerializeField] float horizontalMove = 20f;
+    [SerializeField] float verticalMove = 20f;
+
     Vector2 movement;
 
     void Update()
     {
         ProcessTranslating();
+        ProcessRotating();
     }
 
     void OnMove(InputValue value)
@@ -23,7 +28,7 @@ public class PlayerMovement : MonoBehaviour
         movement = value.Get<Vector2>();
     }
 
-    private void ProcessTranslating()
+    void ProcessTranslating()
     {
         float xOffset = movement.x * movespeed * Time.deltaTime;
         float rawXPos = transform.localPosition.x + xOffset;
@@ -34,5 +39,13 @@ public class PlayerMovement : MonoBehaviour
         float clampedYPos = Mathf.Clamp(rawYPos, -yClampRangeDown, yClampRangeUp);
 
         transform.localPosition = new Vector3(clampedXPos, clampedYPos, 0f);
+    }
+
+    void ProcessRotating()
+    {
+        float horizontalValue = horizontalMove * -movement.x;
+        float verticalValue = verticalMove * -movement.y;
+        Quaternion targetRotation = Quaternion.Euler(verticalValue, 0f, horizontalValue);
+        transform.localRotation = targetRotation;
     }
 }
