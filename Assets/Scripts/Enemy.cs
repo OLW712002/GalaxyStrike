491f6de0bbbb0788dae8e9235da3cac50bf44760
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2025-07-10 10:27:35 +0700

    Add explosion logic for enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject destroyedVFX;
+
     private void OnParticleCollision(GameObject other)
     {
+        GameObject vfx = Instantiate(destroyedVFX, transform.position, Quaternion.identity);
+        Destroy(vfx, 3f);
         Destroy(gameObject);
     }
 }
