fff95eae7935b2051a348e3790fa5f708abb5e4d
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2025-07-15 17:36:24 +0700

    Add enemy health

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,11 +3,21 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject enemyDestroyedVFX;
+    [SerializeField] int enemyHealth = 3;
 
     private void OnParticleCollision(GameObject other)
     {
-        GameObject vfx = Instantiate(enemyDestroyedVFX, transform.position, Quaternion.identity);
-        Destroy(vfx, 3f);
-        Destroy(gameObject);
+        ProcessHit();
+    }
+
+    private void ProcessHit()
+    {
+        enemyHealth--;
+        if (enemyHealth <= 0)
+        {
+            GameObject vfx = Instantiate(enemyDestroyedVFX, transform.position, Quaternion.identity);
+            Destroy(vfx, 3f);
+            Destroy(gameObject);
+        }
     }
 }
