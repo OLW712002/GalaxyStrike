46ccda096d2fbd66d7a161c6ec424b11575cf31f
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2025-07-17 19:08:03 +0700

    Add scoreboard UI

--- Assets/Scripts/Scoreboard.cs
+++ Assets/Scripts/Scoreboard.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
+using TMPro;
 
 public class Scoreboard : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI scoreText;
+
     int score = 0;
 
     public void IncreaseScore(int value)
     {
         score += value;
+        scoreText.text = "Score: " + score;
     }
 }
